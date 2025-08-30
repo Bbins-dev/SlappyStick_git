4ca9e506dc63167814a442a6c1f90f3155545207
서보빈 <bobinsuh@gmail.com>
2025-08-30 15:00:59 +0900

    Floor 오브젝트 에디터에서 선택 안되게 처리
    실수를 줄이기 위해

---
+++ Assets/Editor/FloorEditorLock.cs
@@ -0,0 +1,30 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Prevents all GameObjects with the 'Floor' tag from being selected or moved in the Unity Editor.
+/// This is applied automatically for all team members.
+/// </summary>
+[InitializeOnLoad]
+public static class FloorEditorLock
+{
+    static FloorEditorLock()
+    {
+        EditorApplication.hierarchyChanged += LockFloorObjects;
+        LockFloorObjects();
+    }
+
+    static void LockFloorObjects()
+    {
+        foreach (var floor in GameObject.FindGameObjectsWithTag("Floor"))
+        {
+            if ((floor.hideFlags & HideFlags.NotEditable) == 0)
+            {
+                floor.hideFlags |= HideFlags.NotEditable;
+                EditorUtility.SetDirty(floor);
+            }
+        }
+    }
+}
+#endif
